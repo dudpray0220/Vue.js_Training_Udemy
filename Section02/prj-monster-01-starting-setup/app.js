63be126ceb8c3b3de73bde9e6f2158cbc38d30f0
YoungHyun Bae <qodudgus0220@naver.com>
2023-06-08 11:18:28 +0900

    # 5, 체력바 수정, 뉴게임, 항복 버튼 개발

--- Section02/prj-monster-01-starting-setup/app.js
+++ Section02/prj-monster-01-starting-setup/app.js
@@ -8,14 +8,41 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            turn: 1, // 특수공격은 3턴 마다 1번
+            turn: 1, // 특수공격은 3턴 마다 1번,
+            winner: null // falsy 취급
         }
+    },
+    watch: {
+      playerHealth(value) {
+          if (value <= 0 && this.monsterHealth <= 0) {
+              // Draw
+              this.winner = 'draw';
+          } else if (value <= 0) {
+              // Player Lose
+              this.winner = 'monster';
+          }
+      },
+      monsterHealth(value) {
+          if (value <= 0 && this.playerHealth <= 0) {
+              // Draw
+              this.winner = 'draw';
+          } else if (value <= 0) {
+              // Monster Lose
+              this.winner = 'player';
+          }
+      }
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return {width: '0%'}
+            }
             return {width: `${this.monsterHealth}%`};
         },
         playerBarStyles() {
+        if (this.playerHealth < 0) {
+            return {width: '0%'}
+        }
             return {width: this.playerHealth + '%'};
         },
         specialAttackReady() {
@@ -26,6 +53,13 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        startGame() {
+            // reset data
+            this.playerHealth= 100;
+            this.monsterHealth= 100;
+            this.turn= 1; // 특수공격은 3턴 마다 1번,
+            this.winner= null; // falsy 취급
+        },
         attackMonster() {
             this.turn ++;
             const attackValue = getRandomValue(5, 12);
@@ -51,6 +85,9 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster';
         }
     }
 });
