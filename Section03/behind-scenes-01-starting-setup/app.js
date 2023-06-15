58d68a6d010773be869d35567f227da167a070e8
YoungHyun Bae <qodudgus0220@naver.com>
2023-06-15 11:50:11 +0900

    # Section 3 Vue 생명주기 훅 연습

--- Section03/behind-scenes-01-starting-setup/app.js
+++ Section03/behind-scenes-01-starting-setup/app.js
@@ -16,8 +16,36 @@ const app = Vue.createApp({
       console.dir(this.$refs.userText);
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate!');
+  },
+  created() {
+    console.log('created()')
+  },
+  beforeMount() {
+    console.log('beforeMount()')
+  },
+  mounted() {
+    console.log('mounted()')
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate')
+  },
+  updated() {
+    console.log('updated()')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()')
+  },
+  unmounted() {
+    console.log('unmounted()')
+  }
 });
+
 app.mount('#app');
+setTimeout(function () {
+  app.unmount();
+}, 3000)
 
 const app2 = Vue.createApp({
   template: `
@@ -48,4 +76,4 @@ const handler = {
 };
 const proxy =  new Proxy(data, handler);
 proxy.message = 'Hello!!!!!!'
-console.log(proxy.longMessage);
+// console.log(proxy.longMessage);
