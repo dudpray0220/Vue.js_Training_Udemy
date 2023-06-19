edc2589810ef9d6561546f9070bc55f428b1d816
YoungHyun Bae <qodudgus0220@naver.com>
2023-06-19 22:15:12 +0900

    # Section 4 component 연습

---
+++ Section04/first-app/src/components/FriendContact.vue
@@ -0,0 +1,40 @@
+<template>
+	<li>
+		<h2>{{ name }}</h2>
+		<button @click="toggleDetails">
+			{{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+		</button>
+		<ul v-if="detailsAreVisible">
+			<li><strong>Phone:</strong> {{ phoneNumber }}</li>
+			<li><strong>Email:</strong> {{ emailAddress }}</li>
+		</ul>
+	</li>
+</template>
+
+
+<script>
+export default {
+  props: [
+      'name',
+      'phoneNumber',
+      'emailAddress'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friends: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manel@localhost.com'
+      }
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+}
+</script>
+
