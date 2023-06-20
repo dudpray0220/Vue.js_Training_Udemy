3921e69c961f6bf28d13aab772f1d2ed0eef9c10
YoungHyun Bae <qodudgus0220@naver.com>
2023-06-20 14:24:43 +0900

    # Section 4 component props, v-for 연습

--- Section04/first-app/src/components/FriendContact.vue
+++ Section04/first-app/src/components/FriendContact.vue
@@ -17,12 +17,48 @@
 
 <script>
 export default {
-  props: [
-      'name',
-      'phoneNumber',
-      'emailAddress',
-      'isFavorite'
-  ],
+  // 최소한의 구성
+  // props: [
+  //     'name',
+  //     'phoneNumber',
+  //     'emailAddress',
+  //     'isFavorite'
+  // ],
+
+  // 객체로 타입 알려주기
+  // props: {
+  //   name: String,
+  //   phoneNumber: String,
+  //   emailAddress: String,
+  //   isFavorite: String
+  // },
+
+  // 객체로 더 자세한 정보 알려주기
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false, // 필수가 아닌 props는 기본값 지정가능.
+      // default: function() {} // 함수도 실행 가능.
+      // 참, 거짓을 반환하는 함수는 검증자 추가가능. props에 제공되는 값을 가져와서 유효성 검사 로직으로 유효한 값인지 확인
+      // validator: function(value) {
+      //   return value === '1' || value === '0' // 1 또는 0이라면 true, 아니면 false
+      //   }
+      }
+    },
+
   data() {
     return {
       detailsAreVisible: false,
